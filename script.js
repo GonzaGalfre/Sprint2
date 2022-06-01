cf8f597b9757b21ce13bc496a4085c298407827d
GonzaGalfre <gonzaa.galfre@gmail.com>
2022-06-01 01:50:41 -0300

    Se posiciona horizontalmente el historial

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ function add() {
     let name = $("#userName").val();
     let money = $("#userMoney").val();
     cont += 1;
+    
     distribute(name, money, cont);
 
     let nameToShow = `<h3> ${name} </h3>`
     let moneyToShow = `<h3> ${money} </h3>`
-    $("#history").append(nameToShow, moneyToShow);
     
+    $("#historyName").append(nameToShow);
+    $("#historyMoney").append(moneyToShow);
 }
 
 function distribute(person, spent, c) {
